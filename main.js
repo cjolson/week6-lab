bc8e06d224f63688cef9107ec2934892e6d333f2
Clara Olson <cjolson@andrew.cmu.edu>
2018-10-01 10:49:51 -0400

    First commit for lab 6

---
+++ main.js
@@ -0,0 +1,18 @@
+function addNewList() {
+    alert('hello world!');
+}
+
+function addListItem() {
+    console.log('hello world');
+    var list = document.getElementById("grocery-list");
+    var itemInput = document.getElementById("new-list-item");
+    var newItem = document.createElement("li");
+    newItem.appendChild(document.createTextNode(itemInput.value));
+    list.appendChild(newItem);
+}
+
+function deleteListItem(item) {
+    // remove li element (item) from ol element (item.parentNode)
+    console.log('remove item');
+    item.parentNode.removeChild(item);
+}
